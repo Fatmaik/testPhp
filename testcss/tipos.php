02aca569e53a80792fce006d62a37b6c6b397fde
Dionathan Cordova <dionathan_bass@hotmail.com>
2017-01-12 14:36:49 -0200

    number_formact add

--- testcss/tipos.php
+++ testcss/tipos.php
@@ -10,25 +10,27 @@
 <body>
     <div class='t'>   
         <?php
-            $nota1 = $_GET["nota1"];
-            $nota2 = $_GET["nota2"];
-            $media = ($nota1+$nota2)/2;
-
+            $nota1 = number_format($_GET["nota1"], "1");
+            $nota2 = number_format($_GET["nota2"], "1");
+            $media = number_format(($nota1+$nota2)/2, "1");
             if($media < 5) {
                 $res = "REPROVADO";
-            }else if($media >= 5 || $media < 7) {
+                $color = "#f00";
+            }else if($media >= 5 && $media < 7) {
                 $res = "RECUPERAÇAO";
-            }else {
+                $color= "#00f";
+            }else if($media >=7) {
                 $res = "APROVADO";
+                $color ="#33CC00";
             }
-
-            echo "A media entre $nota1 e $nota2:<br>Situação do Aluno: $res";
+            echo "A media entre <span style='color: #FF8B52'>$nota1</span> e
+                <span style='color: #FF8B52'> $nota2</span> :<br>Situação do Aluno: <span style='color:$color'>$res</span>";
             
         ?>
         <br>
-        <a href="http://localhost/CursoPHP/curso/tipos.html"><button type="button">Voltar</button></a>
-    </div>
+        <a href="http://localhost/CursoPHP/testPhp/testcss/tipos.html"><button type="button">Voltar</button></a>
+</div>
 
     
 </body>
-</html>
+</html
