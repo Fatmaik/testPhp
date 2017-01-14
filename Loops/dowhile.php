99812f80317f6ef7368f6fff13c73a0d1a8f48df
Dionathan Cordova <dionathan_bass@hotmail.com>
2017-01-14 16:03:05 -0200

    dowhile

---
+++ Loops/dowhile.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <div>
+        <?php
+            $inicio = $_GET["num1"];
+            $final = $_GET["num2"];
+            $inc = $_GET["inc"];
+
+            echo "Numero inicial: $inicio<br>";
+            echo "Numero Final: $final<br>";
+            echo "Numero de incremento: $inc<br>";
+
+            
+            if($inicio === $final){
+                echo "NUMEROS IDENTICOS!";
+
+            }else if($inicio < $final) {
+                $i = $inicio;
+                do{
+                    echo "$i ";
+                    $i += $inc;
+                }while($i <= $final);
+
+            }else if($inicio > $final) {
+                $i = $inicio;
+                do{
+                    echo "$i ";
+                    $i -= $inc;
+                }while($i >= $final);
+            }
+            
+
+               
+         ?>
+        <br><br>
+        <a href="javascript:history.go(-1)" class="button">Back</a>
+        
+    </div>
+</body>
+</html>
---
+++ Loops/fatorial.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <div>
+        <?php
+           
+               
+         ?>
+        <br><br>
+        <a href="javascript:history.go(-1)" class="button">Back</a>
+        
+    </div>
+</body>
+</html>
