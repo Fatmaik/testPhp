3aa4756e53bd89a77267d83e56fc2387732d3bad
Dionathan Cordova <dionathan_bass@hotmail.com>
2017-01-14 17:30:48 -0200

    Fatorial

--- Loops/fatorial.php
+++ Loops/fatorial.php
@@ -8,8 +8,26 @@
 <body>
     <div>
         <?php
+           $num = $_GET["num"];
            
-               
+           // var pega valor de $num para fazer a subtracao
+           $i = $num;
+           // var para calcular a multiplicacao do fatorial
+           $x = 1;
+
+           echo "Fatorial de $num: ";
+           do{
+               // var pega o valor de $num e subtrai 1
+               $fat = "$i x ";
+               $i--;
+               $x *= $fat;
+              
+               if($fat == 1){
+                    $fat = ($i+1)." = ";
+               }
+               echo $fat;
+            }while($i > 0);
+            echo $x;
          ?>
         <br><br>
         <a href="javascript:history.go(-1)" class="button">Back</a>
