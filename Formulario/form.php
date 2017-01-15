3688aec8ec0f4724677bfe7035b93924c74428cf
Dionathan Cordova <dionathan_bass@hotmail.com>
2017-01-15 11:42:53 -0200

    primeiro versao form iniciante

--- Formulario/form.php
+++ Formulario/form.php
@@ -8,8 +8,15 @@
 </head>
 <body>
     <div>
+        
     <?php
-        $nome = 'null' != $_GET["nome"]?$_GET["nome"]: "Dado Incorreto";
+    // este formulario esta mal configurado  apos aprender RegExp farei a versao 2;
+        $nome = isset($_GET["nome"])?$_GET["nome"]:"Nome: Dado Incorreto";
+        $tel = isset($_GET["tel"])?$_GET["tel"]:"Telefone: Dado Incorreto";
+        $email = isset($_GET["email"])?$_GET["email"]:"Email: Dado Incorreto";
+
+        echo "<span>Nome: $nome<br>
+        Telefone: $tel<br>Email: $email</span>";
         
     ?>
     <br><br>
