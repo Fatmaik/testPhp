9d6d614226a5f123c9e60ee66a6cc838b6df2936
Dionathan Cordova <dionathan_bass@hotmail.com>
2017-01-19 15:20:51 -0200

    validaceo level 1

--- Formulario/form.php
+++ Formulario/form.php
@@ -15,8 +15,20 @@
         $tel = isset($_GET["tel"])?$_GET["tel"]:"Telefone: Dado Incorreto";
         $email = isset($_GET["email"])?$_GET["email"]:"Email: Dado Incorreto";
 
-        echo "<span>Nome: $nome<br>
-        Telefone: $tel<br>Email: $email</span>";
+        $info = True;
+        if($nome != is_numeric($nome) && $tel == is_numeric($tel)){
+            $info = True;
+        }
+        else{
+            $info = False;
+            echo"Dados Incorretos";
+        };
+        if($info != False) {
+            echo "<span class='foco'>Nome: $nome<br>
+                                    Telefone: $tel<br>
+                                    Email: $email";
+        }
+        
         
     ?>
     <br><br>
