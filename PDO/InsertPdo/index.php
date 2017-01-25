2d6b99228c987bf651f73d1bd623ce6ab11e9e61
Dionathan Cordova <dionathan_bass@hotmail.com>
2017-01-25 17:46:54 -0200

    Insert into

---
+++ PDO/InsertPdo/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <div>
+        <form action="index.php" method="post">
+            <table style="border : 1px solid black; padding: 4px">
+                <caption>Area de Cadastro</caption>
+                <tr>
+                    <td>Loguin</td>
+                    <td><input type="text" name="nome"></td>
+                </tr>
+                <tr>
+                    <td>Senha</td>
+                    <td><input type="password" name="pass"></td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td><input type="email" name="email"></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" class="button"></td>
+                </tr>
+            </table>    
+        </form>    
+
+        <?php
+            $dsn = "mysql:dbname=usuario;host=127.0.0.1";
+            $dbuser = "root";
+            $dbpass = "";
+            
+            try{
+                $pdo = new PDO($dsn, $dbuser, $dbpass);
+
+                $nome = $_POST["nome"] != ""?$_POST["nome"]:"Inforne Nome";
+                $pass = md5($_POST["pass"]) != ""?md5($_POST["pass"]):"Informe Senha";
+                $email = $_POST["email"] != ""?$_POST["email"]:"Informe Email";
+
+                $sql = "INSERT INTO usuarios SET id = default, loguin = '$nome', senha = '$pass', email = '$email'";
+                $sql = $pdo->query($sql);
+
+                echo "Cadasto concluido";
+            }catch(PDOExcepition $e) {
+                echo "Fail: " . $e->getmessage();
+            }
+
+        
+        ?>
+    </div>
+</body>
+</html>
--- PDO/SecondPdo/index.php
+++ PDO/SecondPdo/index.php
@@ -8,20 +8,20 @@
 <body>
     <div>
         <?php
-            $dsn = "mysql:dbname=world;host=localhost"; //acessa o banco
+            $dsn = "mysql:dbname=world;host=localhost;charset=utf8"; //acessa o banco
             $dbuser = "root"; //loguin para acesso ao banco
             $dbpass = ""; //senha para acesso 
 
             try{
                 $pdo = new PDO($dsn, $dbuser, $dbpass); // required andress of db as user/pass too;
-                $sql = $pdo->query("select * from city"); // command to search in db
+                $sql = $pdo->query("select * from city where CountryCode = 'BRA'"); // command to search in db
 
                 if($sql->rowCount() > 0) { // if empty request else will be returned;
                     // receive all request of command as one array, use this only to learning;
                     // try with fletchAll() to absorv;
                     $sqlInfo = $sql->fetchAll(); 
                     foreach($sqlInfo as $info) {
-                        echo "Info: " . $info["District"] . "<br>";
+                        echo "Info: " . $info["District"] . " - " . $info["CountryCode"] .  "<br>";
                     } 
                 }else {
                     echo "Invalid Request";
