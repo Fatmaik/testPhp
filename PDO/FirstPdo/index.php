5b01a3f5a36232c806b5f7c645fde79e0fc856ad
Dionathan Cordova <dionathan_bass@hotmail.com>
2017-01-25 11:00:52 -0200

    first PDO

---
+++ PDO/FirstPdo/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <div>
+        <?php
+            $dsn = "mysql:dbname=world;host=127.0.0.1";
+            $dbuser = "root";
+            $dbpass = "";
+
+            try{
+                $pdo = new PDO($dsn, $dbuser, $dbpass);
+                echo "Conexao Concluida";
+            }catch(PDOException $e) {
+                echo "Falhou: ".$e->getMessage();
+            }
+        ?>
+        
+    </div>
+    
+</body>
+</html>
