683650c312c6dcecee9473dcb807a1d63c2133e0
Dionathan <dionathan_bass@hotmail.com>
2017-03-16 15:34:01 -0300

    composer done

--- PooAvancado/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'conexao.php';
-require_once 'classCurso.php';
-require_once 'container.php';
-
-$con = Container::getConexao();
-$cur = Container::getCurso();
-var_dump($cur->selectAll());
-
