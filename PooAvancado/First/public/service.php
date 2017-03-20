c7adea799d89d748cb903a0bf75a4a37d5b64b4c
Dionathan Cordova <dionathan_bass@hotmail.com>
2017-03-20 11:45:59 -0300

    Update service.php

--- PooAvancado/First/public/service.php
+++ PooAvancado/First/public/service.php
@@ -5,7 +5,7 @@ use Pimple\Container;
 $container = new Container();
 
 $container['conexao'] = function() {
-    return new \Source\Conexao("mysql:dbname=prog;host=localhost", "root", "rancid");
+    return new \Source\Conexao("mysql:dbname=prog;host=localhost", "root", "root");
 };
 $container['curso'] = function($c) {
     return new \Source\Curso($c['conexao']);
