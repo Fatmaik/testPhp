956cf4c0ae9b0d9d3bc549577a2fe8d9a6fcfe4c
Dionathan <dionathan_bass@hotmail.com>
2017-03-25 14:12:41 -0300

    first stap done(show info)

--- PooAvancado/Paginacao/index.php
+++ PooAvancado/Paginacao/index.php
@@ -2,11 +2,43 @@
 require_once 'pagina.php';
 require_once 'conexao.php';
 
-$c = new Conexao("mysql:dbname=world;host=localhost", "root", "rancid");
-$c->connect();
-
-$x = new pag($c);
-$cc = $x->sel();
-foreach($cc as $info) {
-    echo $info['Name'] . "<br>";
+function id($i) {
+    // instancia da classe de conexao 
+    $conn = new Conexao("mysql:dbname=world;host=localhost", "root", "rancid");
+    
+    // instancia da classe de paginas recebendo classe Conexao como parametro
+    $paginas = new Pag($conn);
+    
+    // classe Paginas chamando o metodo de SELECT no Database
+    $allPages = $paginas->sel();
+    foreach($allPages as $info) {
+        echo $info[$i]."<br>";
+    }
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <table border='2px' colspan='3'>
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Distrito</th>
+            <th>Population</th>
+        </tr>
+        <tr>
+            <td><?php id("ID"); ?></td>
+            <td><?php id("Name"); ?></td>
+            <td><?php id("District"); ?></td>
+            <td><?php id("Population"); ?></td>
+        </tr>
+    </table>
+</body>
+</html>
--- PooAvancado/Paginacao/pagina.php
+++ PooAvancado/Paginacao/pagina.php
@@ -1,21 +1,16 @@
 <?php
 require_once 'conexao.php';
-class pag{
+class Pag{
     private $pdo;
 
     public function __construct($pdo){
         return $this->pdo = $pdo->connect();
-
     }
-
     public function sel() {
         $p = 1;
-        $sel = "SELECT * from city limit 1,20 ";
+        $sel = "SELECT * from city limit 0,20 ";
         $query = $this->pdo->prepare($sel);
         $query->execute();
         return $query->fetchAll(\PDO::FETCH_ASSOC);
     }
-
-
-
 }
