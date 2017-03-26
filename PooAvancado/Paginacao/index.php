afcd8e3ef399f3d52018682e8cc7fb1246e6a724
Dionathan <dionathan_bass@hotmail.com>
2017-03-25 22:36:28 -0300

    test link stap 1

--- PooAvancado/Paginacao/index.php
+++ PooAvancado/Paginacao/index.php
@@ -1,20 +1,23 @@
 <?php
 require_once 'pagina.php';
 require_once 'conexao.php';
-
+$conn = new Conexao("mysql:dbname=world;host=localhost", "root", "rancid");
+$paginas = new Pag($conn);
 function id($i) {
     // instancia da classe de conexao 
     $conn = new Conexao("mysql:dbname=world;host=localhost", "root", "rancid");
-    
     // instancia da classe de paginas recebendo classe Conexao como parametro
     $paginas = new Pag($conn);
-    
     // classe Paginas chamando o metodo de SELECT no Database
-    $allPages = $paginas->sel();
+   
+    $allPages = $paginas->sel(2);
     foreach($allPages as $info) {
         echo $info[$i]."<br>";
     }
+    
 }
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -40,5 +43,8 @@ function id($i) {
             <td><?php id("Population"); ?></td>
         </tr>
     </table>
+
+    
+    <a href=""><?php $paginas->lin();?></a>
 </body>
 </html>
--- PooAvancado/Paginacao/pagina.php
+++ PooAvancado/Paginacao/pagina.php
@@ -2,15 +2,32 @@
 require_once 'conexao.php';
 class Pag{
     private $pdo;
+    private $p;
 
     public function __construct($pdo){
         return $this->pdo = $pdo->connect();
     }
-    public function sel() {
-        $p = 1;
-        $sel = "SELECT * from city limit 0,20 ";
+    public function setP($p) {
+        $this->p = $p;
+    }
+    public function getP() {
+        return $this->p;
+    }
+
+    public function sel($p) {
+        
+        $sel = "SELECT * from city limit $p,20 ";
         $query = $this->pdo->prepare($sel);
         $query->execute();
         return $query->fetchAll(\PDO::FETCH_ASSOC);
     }
+
+    public function lin() {
+
+        for($q=0; $q<40; $q++) {
+            echo $q . "-";
+        }
+        
+    }
+    
 }
