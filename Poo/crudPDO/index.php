710a048fcdd81088d8642fdfd70e6ef2263067e4
Dionathan de Cordova <dionathan_bass@hotmail.com>
2017-05-28 15:59:27 -0300

    Delete index.php

--- Poo/crudPDO/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'conexao.php';
-require_once 'classCurso.php';
-
-$con = new Conexao("mysql:dbname=prog;host=localhost", "root", "rancid");
-
-$curso = new Curso($con);
-
-var_dump($curso->selectAll());
